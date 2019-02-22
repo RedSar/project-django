3883fbdf1702c375ec3c05ac9451ab47620bd160
Radouane GUERCHAL <guerchal.radouane@gmail.com>
2019-02-22 01:09:30 +0100

    setting the User Registration.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def login(req):
@@ -11,10 +12,33 @@ def login(req):
 
 def register(req):
     if req.method == 'POST':
-        messages.error(req, 'An error occurs !!')
-        messages.info(req, 'this is an info alert')
-        messages.warning(req, 'this is warning alert')
-        messages.success(req, 'this is success alert')
+        username = req.POST['username']
+        first_name = req.POST['first_name']
+        last_name = req.POST['last_name']
+        email = req.POST['email']
+        password = req.POST['password']
+        password2 = req.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(req, 'this username is taken !')
+            elif User.objects.filter(email=email).exists():
+                messages.error(req, 'this email is being used !')
+            else:
+                # we're good to go and register the user:
+                user = User.objects.create_user(username=username,
+                                                first_name=first_name,
+                                                last_name=last_name,
+                                                email=email,
+                                                password=password,
+                                                )
+                user.save()
+                messages.success(
+                    req, 'You are now successfuly registred and can log in')
+                return redirect('login')
+
+        else:
+            messages.error(req, 'Passwords do not match !')
 
         return redirect('register')
 
